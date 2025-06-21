2628cbb6133ddaff1184a52af3c985a3f6517ccb
Saran Mannam <mannamsaran2001@gmail.com>
2025-06-21 14:20:10 -0400

    Sum of Two Numbers

---
+++ sumofaandb.java
@@ -0,0 +1,10 @@
+public class sumofaandb {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        // Calculate the sum of a and b
+        int sum = a + b;
+        // Print the sum to the console
+        System.out.println("the sum of a and b is: " + sum);
+    }   
+}
