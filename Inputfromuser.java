6a3c39f2c39cbad610b2ab9234a4102a69a95356
Saran Mannam <mannamsaran2001@gmail.com>
2025-06-23 15:18:10 -0400

    sum of two numbers taking input from User

--- Inputfromuser.java
+++ Inputfromuser.java
@@ -2,9 +2,11 @@ import java.util.*;
 public class Inputfromuser {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your input: ");
+        System.out.print("Enter your name: ");
         String input = sc.nextLine();
-        System.out.println(input);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Hello " + input + ", you are " + age + " years old.");
     }
     
 }
