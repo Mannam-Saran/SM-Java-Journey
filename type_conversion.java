a654e3b27531165f322c0de85946b72ad6422fd4
Saran Mannam <mannamsaran2001@gmail.com>
2025-06-23 16:39:43 -0400

    Delete type_conversion.java

--- type_conversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class type_conversion {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        float num= sc.nextInt();
-        System.out.println("The number you entered is: " + num);
-    } 
-}
