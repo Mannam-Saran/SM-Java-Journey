485163535f5cd365ae7f5e5d681719dc4722431d
Saran Mannam <mannamsaran2001@gmail.com>
2025-06-23 16:34:02 -0400

    Type Casting

---
+++ typecasting.java
@@ -0,0 +1,7 @@
+public class typecasting {
+    public static void main(String args[]) {
+        float num= 25.999f;
+        int num1= (int) num;
+        System.out.println("The number you entered is: " + num1);
+    }
+}
